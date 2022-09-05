3fa363144dad4117c33024edce141cc29b954c6d
zehour <zehourchaoui@gmail.com>
2022-09-04 19:54:36 -0700

    login form done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
+// function App() {
+//   return (
+//     <h1>Hello from App</h1>
+//   );
+// }
+// export default App;
+
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
